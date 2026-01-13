15c8ade0e139acf881ec46497b759403f5e38b0b
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-01-13 15:22:12 -0800

    Update initiate_driver.py
    Made changes for this to be back to Google Chrome

--- initiate_driver.py
+++ initiate_driver.py
@@ -3,13 +3,8 @@ from selenium.webdriver.chrome.options import Options
 
 def setDriver(pathToProfile, u_a):
     chrome_options = Options()
-
-    # Use Snap Chromium
-    chrome_options.binary_location = "/snap/bin/chromium"
-
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(f"user-agent={u_a}")
-
     return webdriver.Chrome(options=chrome_options)
