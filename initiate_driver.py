440c1cf841f7eafdf5db1baa593dfa31cc4aab30
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-01-13 12:34:43 -0800

    Update initiate_driver.py

--- initiate_driver.py
+++ initiate_driver.py
@@ -3,6 +3,10 @@ from selenium.webdriver.chrome.options import Options
 
 def setDriver(pathToProfile, u_a):
     chrome_options = Options()
+
+    # Tell Selenium where Chromium lives on GitHub Actions
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
+
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
