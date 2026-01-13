0e76adf70b9fdcc65280539d1e195c653e47d5ae
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-01-13 12:23:21 -0800

    Update initiate_driver.py
    updated initiate_driver.py file

--- initiate_driver.py
+++ initiate_driver.py
@@ -1,16 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 def setDriver(pathToProfile, u_a):
-    #chrome options
-    custom_user_profile = "" # path to custom user profile 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("user-agent="+u_a)
-    # chrome_options.add_argument("user-agent="+u_a) -- custom user profile 
-    #chrome_options.add_arguments("") # for custom headers
-    # set up chrome driver
-    service = Service(get_driver_location())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver 
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"user-agent={u_a}")
+
+    return webdriver.Chrome(options=chrome_options)
+ driver 
