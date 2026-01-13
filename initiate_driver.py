1074b34d33aa8fc8339e20cfd4bd55ff5d0c41e8
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-01-13 15:13:50 -0800

    Update initiate_driver.py
    Update initiate_driver.py for updated chromium usage

--- initiate_driver.py
+++ initiate_driver.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.options import Options
 def setDriver(pathToProfile, u_a):
     chrome_options = Options()
 
-    # Tell Selenium where Chromium lives on GitHub Actions
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    # Use Snap Chromium
+    chrome_options.binary_location = "/snap/bin/chromium"
 
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(f"user-agent={u_a}")
